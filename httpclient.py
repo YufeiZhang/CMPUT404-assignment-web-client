e7116487b1db9e4509ece3f701dc3dd7a5df7b5a
YufeiZhang <philipyufei@gmail.com>
2016-01-23 15:09:12 -0700

    print which function call which function

--- httpclient.py
+++ httpclient.py
@@ -28,30 +28,46 @@ def help():
     print "httpclient.py [GET/POST] [URL]\n"
 
 class HTTPRequest(object):
+    print("in HTTPRequest")
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
 
 class HTTPClient(object):
     #def get_host_port(self,url):
-		# start doing assignment 2 in lab
+	
     def connect(self, host, port):
+        print("in def 1")
         # use sockets!
-        if True:
-            pass
-        return None
+        if not port:
+            port = 80
+
+        outgoingSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        outgoingSocket.connect((host, port))
+
+        return outgoingSocket
 
     def get_code(self, data):
+        print("in def 2")
+        print("The data is printed like this1:")
+        print(data)
+
         return None
 
     def get_headers(self,data):
+        print("in def 3")
+        print("The data is printed like this2:")
+        #print(data.split('\r\n\r\n'))
         return None
 
     def get_body(self, data):
+        print("in def 4")
+        print("The data is printed like this3:")
         return None
 
     # read everything from the socket
     def recvall(self, sock):
+        print("in def 5")
         buffer = bytearray()
         done = False
         while not done:
@@ -63,16 +79,19 @@ class HTTPClient(object):
         return str(buffer)
 
     def GET(self, url, args=None):
+        print("in def 6")
         code = 500
         body = ""
         return HTTPRequest(code, body)
 
     def POST(self, url, args=None):
+        print("in def 7")
         code = 500
         body = ""
         return HTTPRequest(code, body)
 
     def command(self, url, command="GET", args=None):
+        print("in def 8")
         if (command == "POST"):
             return self.POST( url, args )
         else:
@@ -85,6 +104,8 @@ if __name__ == "__main__":
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
+        print(sys.argv[1])
+        print(sys.argv[2])
         print client.command( sys.argv[1], sys.argv[2] )
     else:
         print client.command( command, sys.argv[1] )    
